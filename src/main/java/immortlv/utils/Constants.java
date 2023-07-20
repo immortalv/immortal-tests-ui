f6aa1911016a862ec7a75db1fa08e7808db41ae8
Rostislav Chabria <51484396+RostykCha@users.noreply.github.com>
2023-07-20 20:15:44 +0300

    test workflow

--- src/main/java/immortlv/utils/Constants.java
+++ src/main/java/immortlv/utils/Constants.java
@@ -3,4 +3,6 @@ package immortlv.utils;
 public class Constants {
     public final static String IMMORTAL_URL_STAGE = "https://develop.immortalv.com/";
     public final static String IMMORTAL_URL_PROD = "https://immortalv.com/";
+
+    public final static String IMMORTAL_URL_ = "https://immortalv.com/";
 }
